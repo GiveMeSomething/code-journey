49202a30d97124cf7c11fb0bc010097ebe8d87fd
Hoang Tien Minh <htminh.1001@gmail.com>
2023-10-06 14:40:23 +0700

    feat: finish aoc day 1

--- java/advent-of-code/src/Main.java
+++ java/advent-of-code/src/Main.java
@@ -1,5 +1,20 @@
+import day1.Calories;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        runDay1();
+    }
+
+    private static void runDay1() {
+        Calories caloriesCounter = new Calories();
+
+        var elfItems = caloriesCounter.readElfItems();
+        int maxCalories = caloriesCounter.maxCalories(elfItems);
+        int topThreeCalories = caloriesCounter.topThreeCalories(elfItems);
+
+        System.out.printf("Max calories carrying is %d%n", maxCalories);
+        System.out.printf("Top three calories carrying is %d%n", topThreeCalories);
     }
 }
