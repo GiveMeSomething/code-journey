6d3cf57e6c595268bc9ab54c99b40a08e70f4dad
Hoang Tien Minh <htminh.1001@gmail.com>
2023-10-07 00:19:16 +0700

    feat: finish day 1, part 1 with Python

---
+++ python/advent-of-code/day_one/one.py
@@ -0,0 +1,30 @@
+# To read from input file and solve challenges
+from pathlib import Path
+
+
+def read_item_from_file():
+    itemFile = open("day_one/items.txt", "r", encoding="utf-8")
+    items = []
+
+    sum = 0
+    for item in itemFile.readlines():
+        current_line = item.strip()
+
+        # add as new item when the encounter empty line
+        if current_line == "":
+            items.append(sum)
+            sum = 0
+            continue
+
+        # add to current sum
+        sum += int(current_line)
+
+    return items
+
+
+def max_item(items: list) -> int:
+    max_val = 0
+    for item in items:
+        max_val = max(max_val, item)
+
+    return max_val
--- python/advent-of-code/main.py
+++ python/advent-of-code/main.py
@@ -1,3 +1,11 @@
-msg = "Roll a dice"
+from day_one.one import read_item_from_file, max_item
 
-print(msg)
+
+def day_one():
+    items = read_item_from_file()
+    max_val = max_item(items)
+
+    print(f"The max item is: {max_val}")
+
+
+day_one()
