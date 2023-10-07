9d215620037421243c31a8500d8ce3e56d2c09d8
Hoang Tien Minh <htminh.1001@gmail.com>
2023-10-07 23:17:15 +0700

    feat: add AoC Day 1 solution with TypeScript

--- typescript/advent-of-code/index.ts
+++ typescript/advent-of-code/index.ts
@@ -1,5 +1,17 @@
+import { getMaxSum, getTopThreeSum, readItemsFromFile } from "./dayOne/dayOne";
+
 function main() {
-  console.log("Hello World");
+  execDayOne();
 }
 
 main();
+
+async function execDayOne() {
+  const itemSums = await readItemsFromFile();
+
+  const max = getMaxSum(itemSums);
+  const topThree = getTopThreeSum(itemSums);
+
+  console.log("Max sum", max);
+  console.log("Top three sum", topThree);
+}
