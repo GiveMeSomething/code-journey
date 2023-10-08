938c516d3bad1d3bc96596122fd3857a6a101027
Hoang Tien Minh <htminh.1001@gmail.com>
2023-10-08 17:31:16 +0700

    feat: add solution for AoC Day 3 Part 1 with Python

---
+++ python/advent-of-code/day_three/three.py
@@ -0,0 +1,56 @@
+from typing import List, Tuple
+
+
+def read_rucksack_from_file() -> List[Tuple[str, str]]:
+    file = open("day_three/rucksacks.txt", "r", encoding="utf-8")
+
+    rucksacks = []
+    for line in file.readlines():
+        divider = int(len(line) / 2)
+        first_compartment = line[:divider]
+        second_compartment = line[divider:]
+        rucksacks.append((first_compartment, second_compartment))
+
+    return rucksacks
+
+
+def calculate_duplicate_char(rucksacks: List[Tuple[str, str]]) -> int:
+    sum = 0
+    for rucksack in rucksacks:
+        first_compartment, second_compartment = rucksack
+        first_char_map = {}
+        second_char_map = {}
+
+        print(first_compartment)
+        print(second_compartment)
+
+        dup_char = ""
+
+        for i in range(len(first_compartment)):
+            if second_char_map.get(first_compartment[i]) == None:
+                first_char_map[first_compartment[i]] = False
+            else:
+                dup_char = first_compartment[i]
+                break
+
+            if first_char_map.get(second_compartment[i]) == None:
+                second_char_map[second_compartment[i]] = False
+            else:
+                dup_char = second_compartment[i]
+                break
+
+        print(f"dup char {dup_char} value {char_value(dup_char)}")
+        sum += char_value(dup_char)
+
+    return sum
+
+
+def char_value(input: str) -> int:
+    char_code = ord(input)
+
+    # Lower case letter
+    if char_code >= 97:
+        return char_code - 96
+
+    # Upper case letter
+    return char_code - 64 + 26
--- python/advent-of-code/main.py
+++ python/advent-of-code/main.py
@@ -1,5 +1,6 @@
 from day_one.one import read_item_from_file, max_item, top_three_item
 from day_two.two import read_inputs_from_file, cal_points, cal_point_v2
+from day_three.three import read_rucksack_from_file, calculate_duplicate_char
 
 
 def day_one():
@@ -19,5 +20,13 @@ def day_two():
     print(f"Final point after changing strategy is: {points_v2}")
 
 
+def day_three():
+    rucksacks = read_rucksack_from_file()
+    priority_sum = calculate_duplicate_char(rucksacks)
+
+    print(f"Sum of priorities of all rucksacks is: {priority_sum}")
+
+
 # day_one()
-day_two()
+# day_two()
+day_three()
