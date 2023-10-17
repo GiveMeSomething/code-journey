c03f559cbc5bf8e8dfc103b95f4ffac6bd0336db
Hoang Tien Minh <htminh.1001@gmail.com>
2023-10-17 11:09:36 +0700

    fix: update solution for Adventure of Code Day 7 Part 1. The directories can have the same name, as long as they don't sit in the same directories, so the last solution fail

--- python/advent-of-code/day_7/seven.py
+++ python/advent-of-code/day_7/seven.py
@@ -6,14 +6,14 @@ def calculate_dir_size():
     commands = read_data_from_file("day_7/commands.txt")
 
     # Keep track of directory size
-    dir_size_map = {}
+    dir_size_map = {"/": 0}
 
     # Keep track of nested directory
-    dir_stack = []
+    current_path = "/"
 
     # Parse commands based on their token
     for command in commands:
-        tokens = command.strip().split(" ")
+        tokens = command.strip().split()
         print(tokens)
 
         # $ indicate a command
@@ -21,15 +21,15 @@ def calculate_dir_size():
             match tokens[1]:
                 case "cd":
                     match tokens[2]:
-                        # Command to return to root
                         case "/":
-                            dir_stack.clear()
-                        # Command to go back one directory
+                            # Command to return to root
+                            current_path = "/"
                         case "..":
-                            dir_stack.pop()
-                        # Else, it is command to go to a specific directory
+                            # Command to go back one directory
+                            current_path = current_path[: current_path.rindex("/")]
                         case _:
-                            dir_stack.append(tokens[2])
+                            # Else, it is command to go to a specific directory
+                            current_path = current_path + "/" + tokens[2]
                 case "ls":
                     # Ignore ls command, only care about their result
                     pass
@@ -37,13 +37,17 @@ def calculate_dir_size():
             # If not begin with $, it indicate that this is a command's result
             match tokens[0]:
                 case "dir":
-                    # If there are directories, add them to the map
-                    dir_size_map[tokens[1]] = 0
+                    # If there are directories, add them to the map (if non-existent)
+                    next_path = current_path + "/" + tokens[1]
+                    if dir_size_map.get(next_path) == None:
+                        dir_size_map[next_path] = 0
                     pass
                 case _:
                     # Add the file size for all directory currently in the stack
-                    for dir in dir_stack:
-                        dir_size_map[dir] += int(tokens[0])
+                    temp = current_path
+                    while temp != "":
+                        dir_size_map[temp] += int(tokens[0])
+                        temp = temp[: temp.rindex("/")]
 
     # Print dir_size_map for debugging
     print(dir_size_map)
@@ -51,7 +55,7 @@ def calculate_dir_size():
     # Caluate the sum of all directory that their size at most 100.000
     sum = 0
     for value in dir_size_map.values():
-        if value <= 100000:
+        if value < 100000:
             sum += value
 
     return sum
