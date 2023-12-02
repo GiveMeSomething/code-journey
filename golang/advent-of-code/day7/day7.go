97d293a57865b62279b3a4913278944ab615d8ae
Hoang Tien Minh <htminh.1001@gmail.com>
2023-12-03 00:20:18 +0700

    feat: add function to parse input file into Map

--- golang/advent-of-code/day7/day7.go
+++ golang/advent-of-code/day7/day7.go
@@ -1,1 +1,55 @@
 package day7
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ParseDirTree() map[string]int {
+	file, err := os.Open("day7/files.txt")
+	if err != nil {
+		panic("Cannot open day 7 input file")
+	}
+
+	reader := bufio.NewReader(file)
+
+	fileMap := make(map[string]int)
+	directoryStack := []string{"/"}
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
+
+		tokens := strings.Split(string(line), " ")
+		if tokens[0] == "$" {
+			// Parse commands
+			if tokens[1] == "cd" {
+				switch tokens[2] {
+				case "/":
+					directoryStack = []string{"/"}
+				case "..":
+					directoryStack = directoryStack[:len(directoryStack)-1]
+				default:
+					directoryStack = append(directoryStack, tokens[2])
+				}
+			}
+		} else {
+			// Parse commands' result
+			if tokens[1] == "dir" {
+				continue
+			}
+
+			fileSize, err := strconv.Atoi(tokens[1])
+			if err != nil {
+				continue
+			}
+
+			filePath := strings.Join(directoryStack, "/")
+			fileMap[filePath] = fileSize
+		}
+	}
+}
