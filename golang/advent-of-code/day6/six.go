9376fc02d0d166829e911d2eb1dc30cd2b654244
Hoang Tien Minh <htminh.1001@gmail.com>
2023-12-02 23:40:47 +0700

    feat: add solution for day 6 part 1

---
+++ golang/advent-of-code/day6/six.go
@@ -0,0 +1,62 @@
+package day6
+
+import (
+	"bufio"
+	"os"
+)
+
+func ReadMessageFromFile() string {
+	file, err := os.Open("day6/messages.txt")
+	if err != nil {
+		panic("Cannot open day 6 input file")
+	}
+
+	reader := bufio.NewReader(file)
+
+	// There is only one line in input
+	line, _, err := reader.ReadLine()
+	if err != nil {
+		panic("Cannot read line from day 6 input file")
+	}
+
+	return string(line)
+}
+
+func FindStartOfPacket(input string) int {
+	return findUniqueSequence(input, 4)
+}
+
+func findUniqueSequence(input string, sequenceLength int) int {
+	char_map := make(map[rune]int)
+
+	for i := 0; i < sequenceLength; i++ {
+		current_char := rune(input[i])
+		char_map[current_char] = 1
+	}
+
+	for i := sequenceLength; i < len(input); i++ {
+		// Add current character to map
+		current_char := rune(input[i])
+
+		count, found := char_map[current_char]
+		if !found {
+			char_map[current_char] = 1
+		} else {
+			char_map[current_char] = count + 1
+		}
+
+		previous_char := rune(input[i-sequenceLength])
+		count = char_map[previous_char]
+		if count-1 == 0 {
+			delete(char_map, previous_char)
+		} else {
+			char_map[previous_char] = count - 1
+		}
+
+		if len(char_map) == sequenceLength {
+			return i
+		}
+	}
+
+	return 0
+}
