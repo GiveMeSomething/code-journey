eb3ac3c5a469786f0d69c0aaa432a5026cbb68b2
Hoang Tien Minh <htminh.1001@gmail.com>
2023-12-14 17:08:47 +0700

    fix: update solution for day 1 part 2 to use reverse input in order to find the last digit (text/number)

--- rust/advent-of-code-2023/src/solutions/one.rs
+++ rust/advent-of-code-2023/src/solutions/one.rs
@@ -46,60 +46,49 @@ pub fn calculate_calibration_sum_text(values: &Vec<String>) -> usize {
 
 fn extract_calibration_value(input: &String) -> usize {
     // Regex to match digits
-    let digit_regex = match Regex::new(r"\d") {
-        Ok(regex) => regex,
-        Err(_) => panic!("Invalid regular expression"),
-    };
-
-    let first_digit: isize = digit_regex
-        .find(input.as_str())
-        .unwrap()
-        .as_str()
-        .parse()
-        .expect("Expect a digit");
-
-    let first_digit: usize = digit_regex
-        .find(input)
-        .unwrap()
-        .as_str()
-        .parse()
-        .expect("Expect a digit");
-
-    let rev_input: String = input.chars().rev().collect();
-    let second_digit: usize = digit_regex
-        .find(&rev_input)
-        .unwrap()
-        .as_str()
-        .parse()
-        .expect("Expect a digit");
-
-    // "1" + "2" = 12 = 1 * 10 + 2
-    return first_digit * 10 + second_digit;
-}
-
-fn extract_calibration_value_text(digit_text_map: &HashMap<&str, usize>, input: &String) -> usize {
-    let inner_regex: String = digit_text_map
-        .keys()
-        .fold(String::new(), |a, b| format!("{a}{b}|"));
-    let regex = format!(r"{inner_regex}\d");
-
-    // Regex to match digits and text-based digits
-    let digit_regex = Regex::new(&regex).expect("Invalid regex");
-
-    let matched_texts: Vec<&str> = digit_regex
-        .iter(input.as_str())
-        .map(|res| res.as_str())
-        .collect();
-    println!("{}", input);
-    println!("{:?}", matched_texts);
+    let digit_regex = Regex::new(r"\d").expect("Expect a valid regex");
 
     let digits: Vec<usize> = digit_regex
         .find_iter(input.as_str())
-        .filter_map(|digit| match digit.as_str().parse().ok() {
-            Some(value) => Some(value),
-            None => Some(*digit_text_map.get(digit.as_str()).unwrap()),
-        })
+        .filter_map(|matched_value| matched_value.as_str().parse().ok())
         .collect();
 
+    // "1" + "2" = 12 = 1 * 10 + 2
     return digits[0] * 10 + digits[digits.len() - 1];
+}
+
+fn extract_calibration_value_text(digit_text_map: &HashMap<&str, usize>, input: &String) -> usize {
+    let forward_regex = Regex::new(r"one|two|three|four|five|six|seven|eight|nine|\d")
+        .expect("Should be a valid regular expression");
+
+    let backward_regex = Regex::new(r"eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|\d")
+        .expect("Should be a valid regular expresison");
+
+    let first_digit: usize = match forward_regex.find(&input) {
+        Some(value) => match value.as_str().parse() {
+            Ok(value) => value,
+            Err(_) => *digit_text_map
+                .get(value.as_str())
+                .expect("Should be able to parse"),
+        },
+        None => 0,
+    };
+
+    let reverse_input: String = input.chars().rev().collect();
+    let second_digit: usize = match backward_regex.find(&reverse_input) {
+        Some(value) => match value.as_str().parse() {
+            Ok(value) => value,
+            Err(_) => {
+                let digit_text: String = value.as_str().chars().rev().collect();
+                *digit_text_map.get(digit_text.as_str()).unwrap()
+            }
+        },
+        None => 0,
+    };
+
+    if second_digit == 0 {
+        return first_digit * 11;
+    } else {
+        return first_digit * 10 + second_digit;
+    }
 }
