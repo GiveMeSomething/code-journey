1b58059f6779772f972be70694bc945fd46d815b
Hoang Tien Minh <htminh.1001@gmail.com>
2024-02-21 00:58:19 +0700

    feat: add solution for problem 153

--- rust/leetcode-rs/src/main.rs
+++ rust/leetcode-rs/src/main.rs
@@ -1,21 +1,19 @@
-use solutions::problem_35::search_insert;
+use crate::solutions::problem_153::find_min;
 
 mod solutions;
 
 fn main() {
-    let test_cases: Vec<(Vec<i32>, i32, i32)> = vec![
-        // (vec![1, 3, 5, 6], 5, 2),
-        (vec![1, 3, 5, 6], 2, 1),
-        // (vec![1, 3, 5, 6], 7, 4),
+    let test_cases: Vec<(Vec<i32>, i32)> = vec![
+        (vec![3, 4, 5, 1, 2], 1),
+        (vec![4, 5, 6, 7, 0, 1, 2], 0),
+        (vec![11, 13, 15, 17], 11),
     ];
 
     for test_case in test_cases {
         let nums = test_case.0;
-        let target = test_case.1;
+        let expected = test_case.1;
 
-        let expected = test_case.2;
-        let output = search_insert(nums, target);
-
-        println!("output = {}, expected {}", output, expected);
+        let output = find_min(nums);
+        assert_eq!(output, expected);
     }
 }
--- rust/leetcode-rs/src/solutions/problem_153.rs
+++ rust/leetcode-rs/src/solutions/problem_153.rs
@@ -1,4 +1,44 @@
 #[allow(dead_code)]
 pub fn find_min(nums: Vec<i32>) -> i32 {
-    0
+    let start = 0;
+    let end = nums.len() - 1;
+
+    recursive_find_min(&nums, start, end)
+}
+
+fn recursive_find_min(nums: &Vec<i32>, start: usize, end: usize) -> i32 {
+    if start >= end {
+        return *nums.get(start).unwrap();
+    }
+
+    let pivot: usize = (start + end) / 2;
+    let pivot_value = *nums.get(pivot).unwrap();
+
+    if pivot == 0 {
+        if pivot_value < nums[pivot + 1] {
+            return pivot_value;
+        }
+
+        return recursive_find_min(nums, pivot + 1, end);
+    }
+
+    if pivot == nums.len() - 1 {
+        if pivot_value < nums[pivot - 1] {
+            return pivot_value;
+        }
+
+        return recursive_find_min(nums, start, pivot - 1);
+    }
+
+    if pivot_value < nums[pivot + 1] && pivot_value < nums[pivot - 1] {
+        return pivot_value;
+    }
+
+    let left_min = recursive_find_min(nums, start, pivot - 1);
+    let right_min = recursive_find_min(nums, pivot + 1, end);
+    if left_min < right_min {
+        left_min
+    } else {
+        right_min
+    }
 }
