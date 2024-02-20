d2d9c342fbff0a9cf6e759051fce80987b37eb53
Hoang Tien Minh <htminh.1001@gmail.com>
2024-02-21 00:23:27 +0700

    chore: setup solution file and tests

---
+++ rust/leetcode-rs/src/solutions/problem_153.rs
@@ -0,0 +1,4 @@
+#[allow(dead_code)]
+pub fn find_min(nums: Vec<i32>) -> i32 {
+    0
+}
---
+++ rust/leetcode-rs/tests/problem_153.rs
@@ -0,0 +1,18 @@
+use leetcode::solutions::problem_153::find_min;
+
+#[test]
+fn test() {
+    let test_cases: Vec<(Vec<i32>, i32)> = vec![
+        (vec![3, 4, 5, 1, 2], 1),
+        (vec![4, 5, 6, 7, 0, 1, 2], 0),
+        (vec![11, 13, 15, 17], 11),
+    ];
+
+    for test_case in test_cases {
+        let nums = test_case.0;
+        let expected = test_case.1;
+
+        let output = find_min(nums);
+        assert_eq!(output, expected);
+    }
+}
