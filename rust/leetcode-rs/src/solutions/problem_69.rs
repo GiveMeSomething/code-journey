2df825328b6614f9cd61a5a316d247ab5adfc686
Hoang Tien Minh <htminh.1001@gmail.com>
2024-02-22 17:51:21 +0700

    feat: add solution for problem 69 with Rust

--- rust/leetcode-rs/src/solutions/problem_69.rs
+++ rust/leetcode-rs/src/solutions/problem_69.rs
@@ -1,4 +1,29 @@
 #[allow(dead_code)]
 pub fn my_sqrt(x: i32) -> i32 {
-    0
+    recursive_my_sqrt(x, 0, x as usize)
+}
+
+pub fn recursive_my_sqrt(x: i32, start: usize, end: usize) -> i32 {
+    println!("start = {}, end = {}, value = {}", start, end, end - start);
+    // Resolve 2 elements case
+    if end - start == 1 {
+        if end * end <= x as usize {
+            return end as i32;
+        }
+        return start as i32;
+    }
+
+    let half = (start + end) / 2;
+    let product = half * half;
+
+    let target: usize = x as usize;
+    if product == target {
+        return half as i32;
+    }
+
+    if target > product {
+        return recursive_my_sqrt(x, half.into(), end);
+    }
+
+    return recursive_my_sqrt(x, start, half.into());
 }
