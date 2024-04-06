5c3f4572a655c802dffdd82c7c1d2f1f0ad57675
Hoang Tien Minh <htminh.1001@gmail.com>
2024-04-06 19:45:23 +0700

    feat: add solution for aoc21 day 6 with Rust

---
+++ rust/advent-of-code-2021/src/solutions/six.rs
@@ -0,0 +1,63 @@
+use std::{
+    collections::VecDeque,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+pub fn read_interval_from_file() -> Vec<isize> {
+    let file = File::open("src/inputs/six.txt").expect("Cannot find/open input file for day 6");
+    let reader = BufReader::new(file);
+
+    let mut intervals: Vec<isize> = vec![0; 0];
+    for line in reader.lines() {
+        let current_line = line.expect("Cannot read next line");
+        intervals = current_line
+            .split(",")
+            .map(|part| part.parse().expect("Interval should be a number"))
+            .collect();
+    }
+    return intervals;
+}
+
+pub fn count_lanternfish(intervals: &Vec<isize>, remain: isize) -> isize {
+    let mut sum = 0;
+
+    let mut fish_queue: VecDeque<(isize, isize)> = VecDeque::new();
+    for interval in intervals {
+        fish_queue.push_back((*interval, remain));
+    }
+
+    while let Some((init, remaining_day)) = fish_queue.pop_front() {
+        let mut child_num = 0;
+        if remaining_day >= init + 1 {
+            child_num += 1;
+        }
+        child_num += (remaining_day - init - 1) / 7;
+
+        for i in 0..child_num {
+            fish_queue.push_back((8, remaining_day - init - 1 - i * 7));
+        }
+
+        sum += 1;
+    }
+
+    return sum;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_count_lanternfish() {
+        let test_intervals = vec![3, 4, 3, 1, 2];
+        let test_days = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 18, 80];
+        let test_days_result = [5, 6, 7, 9, 10, 10, 10, 10, 11, 12, 26, 5934];
+
+        for i in 0..test_days.len() {
+            println!("Test case: {} days", test_days[i]);
+            let result = count_lanternfish(&test_intervals, test_days[i]);
+            assert_eq!(result, test_days_result[i]);
+        }
+    }
+}
