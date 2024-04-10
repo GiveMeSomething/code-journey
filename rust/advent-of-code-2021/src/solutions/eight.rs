da254eba2b4b411e32db9010f3b0dc3fe78b1f59
Hoang Tien Minh <htminh.1001@gmail.com>
2024-04-11 00:33:11 +0700

    feat: add solution for aoc21 day8 part2 with Rust

--- rust/advent-of-code-2021/src/solutions/eight.rs
+++ rust/advent-of-code-2021/src/solutions/eight.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -35,6 +36,47 @@ pub fn count_unique_signals(signals: &Vec<(String, String)>) -> usize {
     return sum;
 }
 
+pub fn sum_decode_signal(signals: &Vec<(String, String)>) -> usize {
+    let magic_map: HashMap<usize, usize> = vec![
+        (42, 0),
+        (17, 1),
+        (34, 2),
+        (39, 3),
+        (30, 4),
+        (37, 5),
+        (41, 6),
+        (25, 7),
+        (49, 8),
+        (45, 9),
+    ]
+    .into_iter()
+    .collect();
+
+    let mut sum = 0;
+
+    for signal in signals {
+        let mut char_map = HashMap::new();
+        let (first, second) = signal;
+        for char in first.split(" ").flat_map(|part| part.chars()) {
+            let value = char_map.entry(char).or_insert(0);
+            *value += 1;
+        }
+
+        let mut decode = 0;
+        for part in second.split(" ") {
+            let mut part_sum = 0;
+            for char in part.chars() {
+                part_sum += char_map.get(&char).unwrap_or(&0);
+            }
+            decode = decode * 10 + magic_map.get(&part_sum).unwrap_or(&0);
+        }
+
+        sum += decode;
+    }
+
+    return sum;
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -55,6 +97,26 @@ mod test {
 
         for i in 0..inputs.len() {
             let receive = count_unique_signals(&inputs[i]);
+            assert_eq!(receive, expecteds[i]);
+        }
+    }
+
+    #[test]
+    fn test_sum_decode_signal() {
+        let inputs = vec![
+            vec![(
+                String::from("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb"),
+                String::from("fdgacbe cefdb cefbgd gcbe"),
+            )],
+            vec![(
+                String::from("edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec"),
+                String::from("fcgedb cgb dgebacf gc"),
+            )],
+        ];
+        let expecteds = [8394, 9781];
+
+        for i in 0..inputs.len() {
+            let receive = sum_decode_signal(&inputs[i]);
             assert_eq!(receive, expecteds[i]);
         }
     }
