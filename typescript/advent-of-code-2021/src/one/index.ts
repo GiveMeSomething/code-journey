e9c9f7a831111a3518e0449ae68520d5ee5880a0
Hoang Tien Minh <htminh.1001@gmail.com>
2024-04-27 22:58:17 +0700

    feat: add solution for aoc21 part 1 with TS

---
+++ typescript/advent-of-code-2021/src/one/index.ts
@@ -0,0 +1,54 @@
+import { createReadStream } from "fs";
+import { Interface, createInterface } from "readline";
+import events from "events";
+
+const executeOne = async () => {
+  const depths = await readDepthFromFile();
+
+  const incresingDepth = countIncreasingDepth(depths);
+  console.log("Increasing depth", incresingDepth);
+}
+
+const readDepthFromFile = async (): Promise<number[]> => {
+  let reader: Interface | null = null;
+
+  try {
+    const inputFile = `${process.cwd()}/src/one/input.txt`;
+    reader = createInterface({
+      input: createReadStream(inputFile, {encoding: "utf-8"})
+    });
+  } catch(error) {
+    console.log(error);
+    return [];
+  }
+
+  if(reader == null) {
+    return [];
+  }
+
+  const result: number[] = [];
+  reader.on("line", (line) => {
+    const value = Number(line);
+    if(Number.isNaN(value)) {
+      return;
+    }
+
+    result.push(value);
+  });
+
+  await events.once(reader, "close");
+
+  return result;
+}
+
+const countIncreasingDepth = (depths: number[]): number => {
+  let count = 0;
+  for(let i = 1; i < depths.length; i++) {
+    if(depths[i] > depths[i-1]) {
+      count++;
+    }
+  }
+  return count;
+}
+
+export {executeOne}
