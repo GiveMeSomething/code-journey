d6e05297c9db69d7d52b5ccc9b612cab849d308a
Hoang Tien Minh <htminh.1001@gmail.com>
2024-05-04 23:11:56 +0700

    feat: add solution for aoc21 day5 part1 with TS

--- typescript/advent-of-code-2021/src/five/index.ts
+++ typescript/advent-of-code-2021/src/five/index.ts
@@ -3,8 +3,10 @@ import { getReader } from "../utils/file";
 import { Vent } from "./vent";
 
 export const executeFive = async () => {
-  const result = await readVentFromFile();
-  console.log(result);
+  const vents = await readVentFromFile();
+
+  const intersect = countIntersect(vents);
+  console.log(`Intersect: ${intersect}`);
 };
 
 const readVentFromFile = async () => {
@@ -20,4 +22,38 @@ const readVentFromFile = async () => {
 
   await once(reader, "close");
   return result;
+};
+
+const countIntersect = (vents: Vent[]) => {
+  const pointMap = new Map<string, number>();
+  for (const vent of vents) {
+    if (!vent.isStraight) {
+      continue;
+    }
+
+    if (vent.start[0] === vent.end[0]) {
+      const start = Math.min(vent.start[1], vent.end[1]);
+      const end = Math.max(vent.start[1], vent.end[1]);
+      for (let i = start; i <= end; i++) {
+        const key = `${vent.start[0]}-${i}`;
+        pointMap.set(key, (pointMap.get(key) ?? 0) + 1);
+      }
+      continue;
+    }
+
+    const start = Math.min(vent.start[0], vent.end[0]);
+    const end = Math.max(vent.start[0], vent.end[0]);
+    for (let i = start; i <= end; i++) {
+      const key = `${i}-${vent.start[1]}`;
+      pointMap.set(key, (pointMap.get(key) ?? 0) + 1);
+    }
+  }
+
+  let count = 0;
+  for (const point of pointMap.values()) {
+    if (point >= 2) {
+      count++;
+    }
+  }
+  return count;
 };
