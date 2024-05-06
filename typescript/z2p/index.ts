ff82205dface3704e760eeb7ced3f9f80df22207
Hoang Tien Minh <htminh.1001@gmail.com>
2024-05-06 22:59:21 +0700

    feat: create new span for all request with middleware

--- typescript/z2p/index.ts
+++ typescript/z2p/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import { trace } from "@opentelemetry/api";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const tracer = trace.getTracer("z2p", "0.0.1");
+
+app.use((request, _, next) => {
+  tracer.startActiveSpan(`${request.method} ${request.url}`, (span) => {
+    try {
+      next();
+    } finally {
+      // Making sure that this span will end
+      span.end();
+    }
+  });
+});
 
 app.get("/", (_, response) => {
   response.json({
