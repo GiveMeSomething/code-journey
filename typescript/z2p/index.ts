1564c8e056dd06a2697f1a097dd82fb4ab7d3f4a
Hoang Tien Minh <htminh.1001@gmail.com>
2024-05-06 23:10:46 +0700

    feat: add new endpoint to print hello to route param

--- typescript/z2p/index.ts
+++ typescript/z2p/index.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import { trace } from "@opentelemetry/api";
+import { Span, trace } from "@opentelemetry/api";
+import { NodeSDK } from "@opentelemetry/sdk-node";
 
+// Extend default Request to hold request span
+declare module "express-serve-static-core" {
+  interface Request {
+    span?: Span;
+  }
+}
+
+// Initialize dotenv
 dotenv.config();
 
+// Initialize opentelemetry
+const sdk = new NodeSDK();
+sdk.start();
+
+// Initialize Express server
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,6 +26,7 @@ const tracer = trace.getTracer("z2p", "0.0.1");
 app.use((request, _, next) => {
   tracer.startActiveSpan(`${request.method} ${request.url}`, (span) => {
     try {
+      request.span = span;
       next();
     } finally {
       // Making sure that this span will end
@@ -20,9 +35,23 @@ app.use((request, _, next) => {
   });
 });
 
-app.get("/", (_, response) => {
+app.get("/", (request: Request, response: Response) => {
+  console.log(request.span?.spanContext());
   response.json({
     message: "Hello World",
+  });
+});
+
+app.get("/:name", (request: Request, response: Response) => {
+  console.log(request.span?.spanContext());
+  const name = request.params["name"];
+  if (!name) {
+    response.status(400).json({ message: "Invalid name" });
+    return;
+  }
+
+  response.json({
+    message: `Hello ${name}`,
   });
 });
 
