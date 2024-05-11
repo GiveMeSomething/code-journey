fe0de3732155e5cffccd8d4f85272e867658fadb
Hoang Tien Minh <hmtinh.1001@gmail.com>
2024-05-11 17:32:16 +0700

    feat: add solution for aoc21 day6 part1 with TS

--- typescript/advent-of-code-2021/src/six/index.ts
+++ typescript/advent-of-code-2021/src/six/index.ts
@@ -4,8 +4,18 @@ import { Interface, createInterface } from "readline";
 
 export const executeSix = async () => {
   const code = await readCodeFromFile();
-  console.log(code);
+
+  const illegalPoint = calculateIllegalPoint(code);
+  console.log("Illegal point", illegalPoint);
 };
+
+const tokenMap: Record<string, number> = {
+  ")": 3,
+  "]": 57,
+  "}": 1197,
+  ">": 25137,
+};
+const openTokenList = ["(", "[", "{", "<"];
 
 const readCodeFromFile = async (): Promise<string[]> => {
   const inputPath = `${process.cwd()}/src/six/input.txt`;
@@ -31,4 +41,38 @@ const readCodeFromFile = async (): Promise<string[]> => {
 
   await once(reader, "close");
   return result;
+};
+
+const calculateIllegalPoint = (lines: string[]) => {
+  let points = 0;
+  for (const line of lines) {
+    points += calculateLineIllegalPoint(line);
+  }
+  return points;
+};
+
+const calculateLineIllegalPoint = (line: string): number => {
+  const queue: string[] = [];
+  for (const token of line.split("")) {
+    if (openTokenList.includes(token)) {
+      queue.push(token);
+      continue;
+    }
+
+    const lastToken = queue[queue.length - 1];
+    if (
+      (lastToken !== "(" && token === ")") ||
+      (lastToken !== "[" && token === "]") ||
+      (lastToken !== "{" && token === "}") ||
+      (lastToken !== "<" && token === ">")
+    ) {
+      return tokenMap[token];
+    }
+
+    const data = queue.pop();
+    if (!data) {
+      return tokenMap[token];
+    }
+  }
+  return 0;
 };
