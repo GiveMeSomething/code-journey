cc70e2261e3c6c9c87fcdb9b1edf70a61b76b1ce
Hoang Tien Minh <htminh.1001@gmail.com>
2024-05-13 23:07:12 +0700

    feat: add solution for aoc21 day6 part 1 + 2 with TS

--- typescript/advent-of-code-2021/src/six/index.ts
+++ typescript/advent-of-code-2021/src/six/index.ts
@@ -2,7 +2,12 @@ import { readFile } from "fs/promises";
 
 export const executeSix = async () => {
   const fishes = await readFishFromFile();
-  console.log("Fishes", fishes);
+
+  const fishCount1 = countFish(fishes, 18);
+  console.log("After 80 days", fishCount1);
+
+  const fishCount2 = countFish(fishes, 256);
+  console.log("After 256 days", fishCount2);
 };
 
 const readFishFromFile = async (): Promise<number[]> => {
@@ -24,4 +29,40 @@ const readFishFromFile = async (): Promise<number[]> => {
     .split(",")
     .map((part) => Number(part))
     .filter((part) => !Number.isNaN(part));
+};
+
+export const countFish = (fishes: number[], remaining: number): number => {
+  const resultMap = new Map<string, number>();
+
+  const countSingleFish = (start: number, remaining: number): number => {
+    if (start < 0 || remaining < 0) {
+      return 0;
+    }
+
+    if (remaining <= start) {
+      return 1;
+    }
+
+    const key = `${start}-${remaining}`;
+    if (resultMap.has(key)) {
+      return resultMap.get(key) as number;
+    }
+
+    let count = 1;
+    while (remaining > 0) {
+      remaining = remaining - start - 1;
+      start = 6;
+
+      count += countSingleFish(8, remaining);
+    }
+
+    resultMap.set(key, count);
+    return count;
+  };
+
+  let count = 0;
+  for (const fish of fishes) {
+    count += countSingleFish(fish, remaining);
+  }
+  return count;
 };
