109c91761ce191107b9f25aed04c22c809851f4a
Hoang Tien Minh <minh.hoang@icetea.io>
2024-05-14 23:02:32 +0700

    feat: add solution for aoc21 day10 part2 with Rust

--- rust/advent-of-code-2021/src/solutions/ten.rs
+++ rust/advent-of-code-2021/src/solutions/ten.rs
@@ -24,6 +24,17 @@ pub fn sum_illegal_point(lines: &Vec<String>) -> isize {
     return sum;
 }
 
+pub fn sum_autocomplete_point(lines: &Vec<String>) -> isize {
+    let mut points: Vec<isize> = lines
+        .into_iter()
+        .filter(|line| get_line_illegal_point(line.as_str()) == 0)
+        .map(|line| get_line_autocomplete_point(line.as_str()))
+        .collect();
+
+    points.sort();
+    points[points.len() / 2]
+}
+
 fn get_line_illegal_point(line: &str) -> isize {
     let mut queue: Vec<char> = vec![];
     for token in line.chars() {
@@ -49,6 +60,30 @@ fn get_line_illegal_point(line: &str) -> isize {
     return 0;
 }
 
+fn get_line_autocomplete_point(line: &str) -> isize {
+    let mut queue: Vec<char> = vec![];
+    for token in line.chars() {
+        if token == '(' || token == '[' || token == '{' || token == '<' {
+            queue.push(token);
+            continue;
+        }
+
+        // Assume that the input is always valid
+        queue.pop();
+    }
+
+    let mut point = 0;
+    while queue.len() > 0 {
+        point = point * 5
+            + match queue.pop() {
+                Some(token) => get_char_autocomplete_point(token),
+                None => 0,
+            }
+    }
+
+    return point;
+}
+
 fn get_char_illegal_point(token: char) -> isize {
     match token {
         ')' => 3,
@@ -59,8 +94,20 @@ fn get_char_illegal_point(token: char) -> isize {
     }
 }
 
+fn get_char_autocomplete_point(token: char) -> isize {
+    match token {
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
+        _ => 0,
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use crate::solutions::ten::sum_autocomplete_point;
+
     use super::sum_illegal_point;
 
     #[test]
@@ -78,6 +125,33 @@ mod test {
             let current_expected = expected[i];
 
             let result = sum_illegal_point(&current_input);
+            assert_eq!(
+                result,
+                expected[i],
+                "Expect {} but receive {} for case {:?}",
+                current_expected.to_string(),
+                result,
+                current_input
+            );
+        }
+    }
+
+    #[test]
+    fn test_calculate_autocomplete_point() {
+        let inputs: Vec<&str> = vec![
+            "[({(<(())[]>[[{[]{<()<>>",
+            "[(()[<>])]({[<{<<[]>>(",
+            "(((({<>}<{<{<>}{[]{[]{}",
+            "{<[[]]>}<{[{[{[]{()[[[]",
+            "<{([{{}}[<[[[<>{}]]]>[]]",
+        ];
+        let expected: Vec<isize> = vec![288957, 5566, 1480781, 995444, 294];
+
+        for i in 0..inputs.len() {
+            let current_input = vec![String::from(inputs[i])];
+            let current_expected = expected[i];
+
+            let result = sum_autocomplete_point(&current_input);
             assert_eq!(
                 result,
                 expected[i],
