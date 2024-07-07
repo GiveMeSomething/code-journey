c696571a896159ff987b95d2cf80e689f6e4dcc3
Hoang Tien Minh <htminh.1001@gmail.com>
2024-07-07 17:26:40 +0700

    feat: add counter example

--- solidity/example/src/Counter.sol
+++ solidity/example/src/Counter.sol
@@ -2,13 +2,18 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
+    uint256 public count;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    function get() public view returns (uint256) {
+        return count;
     }
 
-    function increment() public {
-        number++;
+    function inc() public {
+        count += 1;
+    }
+
+    function desc() public {
+        // This function will fail if count = 0, as count is an unsigned integer
+        count -= 1;
     }
 }
