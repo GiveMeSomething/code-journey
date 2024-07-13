ffd4d0534d9b04193e2577b87244859bbecf6932
Hoang Tien Minh <htminh.1001@gmail.com>
2024-07-14 00:07:26 +0700

    feat: basic ERC721

---
+++ solidity/solit/contract/src/ERC721TokenReceiver.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.26;
+
+/// @notice A generic interface for a contract which properly accepts ERC721 tokens.
+/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC721.sol)
+abstract contract ERC721TokenReceiver {
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return ERC721TokenReceiver.onERC721Received.selector;
+    }
+}
