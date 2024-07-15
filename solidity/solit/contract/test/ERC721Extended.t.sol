b0957eb5e7f9a7c3bb15c587bfce6cf227db5e94
Hoang Tien Minh <htminh.1001@gmail.com>
2024-07-15 23:52:30 +0700

    fix: update test beforeEach test

--- solidity/solit/contract/test/ERC721Extended.t.sol
+++ solidity/solit/contract/test/ERC721Extended.t.sol
@@ -12,37 +12,35 @@ contract ERC721ExtendedTest is Test {
 
     address alice = address(0x1);
 
-    function setup() {
+    function setUp() public {
         erc20Token = new ERC20("TastyFish", "TAFI", 18);
 
-        erc721Token = new ERC721Extended("NFT", "NFT");
+        erc721Token = new ERC721Extended("TastyFish", "TastyNFT");
 
         // Set mint price to 1 TAFI
         erc721Token.addPayment(address(erc20Token), 1e18);
     }
 
     // Test contract setup
-    function testName() external {
-        assertEq("NFT", erc721Token.name());
+    function testName() public view {
+        assertEq("TastyFish", erc721Token.name());
     }
 
-    function testSymbol() external {
-        assertEq("NFT", erc721Token.symbol());
+    function testSymbol() public view {
+        assertEq("TastyNFT", erc721Token.symbol());
     }
 
     function testCheckAllowance() external {
         // Mint for alice
-        erc20Token.mint(alice, 1e18);
+        erc20Token.mint(address(this), 1e18);
 
-        assertEq(erc20Token.balanceOf(alice), 1e18);
+        assertEq(erc20Token.balanceOf(address(this)), 1e18);
         assertEq(erc20Token.totalSupply(), 1e18);
 
-        // Alice approve fund for erc721
-        vm.prank(alice);
-        erc20Token.approve(address(this), 1e18);
+        // Approve fund for erc721
+        assertTrue(erc20Token.approve(address(erc721Token), 1e18));
 
         // Check allowance
-        vm.prank(alice);
         assertTrue(erc721Token.checkAllowance(erc20Token));
     }
 }
