89f6614fc749973d60add9e0b590b7337601b746
Hoang Tien Minh <htminh.1001@gmail.com>
2024-07-19 22:48:43 +0700

    feat: setup Wagmi to connect to Sepolia testnet

--- solidity/solit/frontend/src/main.tsx
+++ solidity/solit/frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { WagmiProvider } from "wagmi";
+import { wagmiConfig } from "./config/wagmi.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={wagmiConfig}>
+      <App />
+    </WagmiProvider>
   </React.StrictMode>,
-)
+);
