94c5028e083ba3478e130bc04fd4e738d3427222
Minh Hoang Tien <htminh.1001@gmail.com>
2024-11-09 01:15:17 +0700

    feat: add solution for problem 35

--- rust/leetcode-rs/src/solutions/problem_35.rs
+++ rust/leetcode-rs/src/solutions/problem_35.rs
@@ -1,24 +1,53 @@
 #[allow(dead_code)]
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    let mut start = 0;
-    let mut end = nums.len();
+    let mut start: usize = 0;
+    let mut end: usize = nums.len() - 1;
 
     while start < end {
         let pivot = (start + end) / 2;
-        let pivot_value = nums[pivot];
-
-        if pivot_value == target {
+        if nums[pivot] == target {
             return pivot as i32;
         }
-
-        if target > pivot_value {
+        if target > nums[pivot] {
             start = pivot + 1;
-        }
-
-        if target < pivot_value {
+        } else {
             end = pivot;
         }
     }
 
-    return start as i32;
+    println!("{} {}", start, end);
+
+    if target <= nums[end] {
+        return end as i32;
+    }
+
+    return (end + 1) as i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::search_insert;
+
+    #[test]
+    fn test_search_insert() {
+        let test_cases: Vec<(Vec<i32>, i32, i32)> = vec![
+            (vec![1, 3, 5, 6], 5, 2),
+            (vec![1, 3, 5, 6], 2, 1),
+            (vec![1, 3, 5, 6], 7, 4),
+            (vec![1, 3, 5, 6, 7, 8, 9, 123, 465, 1235], 100, 7),
+            (vec![1, 3, 5, 6, 7, 8, 9, 123, 465, 1235], 400, 8),
+            (vec![1, 3, 5, 6, 7, 8, 9, 123, 465, 1235], 1000, 9),
+            (vec![1, 3], 0, 0),
+            (vec![1, 3, 5, 6], 0, 0),
+            (vec![1], 1, 0),
+        ];
+
+        let mut i = 0;
+        for (input, target, expected) in test_cases {
+            println!("Test case {:?} {} {}", input, target, expected);
+            i = i + 1;
+            let actual = search_insert(input, target);
+            assert_eq!(actual, expected);
+        }
+    }
 }
