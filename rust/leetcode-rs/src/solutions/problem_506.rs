a5cfcd35585bb16b8ff2fb0f9b16e341651873fd
Minh Hoang Tien <htminh.1001@gmail.com>
2024-11-10 01:08:04 +0700

    feat: add solution using built-in sort

--- rust/leetcode-rs/src/solutions/problem_506.rs
+++ rust/leetcode-rs/src/solutions/problem_506.rs
@@ -22,6 +22,28 @@ pub fn find_relative_ranks(score: Vec<i32>) -> Vec<String> {
     result
 }
 
+pub fn find_relative_ranks_stdlib(score: Vec<i32>) -> Vec<String> {
+    let mut input: Vec<(i32, usize)> = vec![];
+    for i in 0..score.len() {
+        input.push((score[i], i));
+    }
+
+    // Descending order
+    input.sort_by(|&a, &b| b.0.cmp(&a.0));
+
+    let mut result: Vec<String> = vec![String::from(""); score.len()];
+    for i in 0..score.len() {
+        let (_, index) = input[i];
+        match i {
+            0 => result[index] = "Gold Medal".to_string(),
+            1 => result[index] = "Silver Medal".to_string(),
+            2 => result[index] = "Bronze Medal".to_string(),
+            value => result[index] = format!("{}", value + 1),
+        };
+    }
+    result
+}
+
 pub fn heapify(input: &mut Vec<(i32, usize)>, root: usize, limit: usize) {
     let left = BTree::left(root);
     let right = BTree::right(root);
@@ -54,7 +76,7 @@ pub fn heap_sort(input: &mut Vec<(i32, usize)>) {
 
 #[cfg(test)]
 mod test {
-    use super::find_relative_ranks;
+    use super::{find_relative_ranks, find_relative_ranks_stdlib};
 
     #[test]
     fn test_find_relative_ranks() {
@@ -71,6 +93,27 @@ mod test {
 
         for (input, expected) in test_cases {
             let actual = find_relative_ranks(input);
+            for i in 0..actual.len() {
+                assert_eq!(actual[i], String::from(expected[i]));
+            }
+        }
+    }
+
+    #[test]
+    fn test_find_relative_ranks_stdlib() {
+        let test_cases: Vec<(Vec<i32>, Vec<&str>)> = vec![
+            (
+                vec![5, 4, 3, 2, 1],
+                vec!["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"],
+            ),
+            (
+                vec![10, 3, 8, 9, 4],
+                vec!["Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"],
+            ),
+        ];
+
+        for (input, expected) in test_cases {
+            let actual = find_relative_ranks_stdlib(input);
             for i in 0..actual.len() {
                 assert_eq!(actual[i], String::from(expected[i]));
             }
