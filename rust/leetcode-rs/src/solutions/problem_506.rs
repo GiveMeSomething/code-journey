022ad0de06f1d75e6f3b258398bf193bb423c54c
Minh Hoang Tien <htminh.1001@gmail.com>
2024-11-10 01:48:22 +0700

    feat: add solution for problem 506 using built-in collections

--- rust/leetcode-rs/src/solutions/problem_506.rs
+++ rust/leetcode-rs/src/solutions/problem_506.rs
@@ -1,3 +1,8 @@
+use std::{
+    collections::{BinaryHeap, HashMap},
+    vec,
+};
+
 use crate::dsa::btree::BTree;
 
 pub fn find_relative_ranks(score: Vec<i32>) -> Vec<String> {
@@ -44,6 +49,34 @@ pub fn find_relative_ranks_stdlib(score: Vec<i32>) -> Vec<String> {
     result
 }
 
+pub fn find_relative_ranks_collection(scores: Vec<i32>) -> Vec<String> {
+    let mut index_mapper: HashMap<i32, usize> = HashMap::new();
+    for i in 0..scores.len() {
+        index_mapper.insert(scores[i], i);
+    }
+
+    let mut heap: BinaryHeap<i32> = BinaryHeap::new();
+    for i in 0..scores.len() {
+        heap.push(scores[i]);
+    }
+
+    let mut result: Vec<String> = vec!["".to_string(); scores.len()];
+    for i in 0..scores.len() {
+        let max_score = heap.pop().expect("get heap root failed");
+        let index = *index_mapper
+            .get(&max_score)
+            .expect("get index from mapper failed");
+
+        match i {
+            0 => result[index] = "Gold Medal".to_string(),
+            1 => result[index] = "Silver Medal".to_string(),
+            2 => result[index] = "Bronze Medal".to_string(),
+            value => result[index] = format!("{}", value + 1),
+        };
+    }
+    result
+}
+
 pub fn heapify(input: &mut Vec<(i32, usize)>, root: usize, limit: usize) {
     let left = BTree::left(root);
     let right = BTree::right(root);
@@ -76,7 +109,7 @@ pub fn heap_sort(input: &mut Vec<(i32, usize)>) {
 
 #[cfg(test)]
 mod test {
-    use super::{find_relative_ranks, find_relative_ranks_stdlib};
+    use super::{find_relative_ranks, find_relative_ranks_collection, find_relative_ranks_stdlib};
 
     #[test]
     fn test_find_relative_ranks() {
@@ -114,6 +147,27 @@ mod test {
 
         for (input, expected) in test_cases {
             let actual = find_relative_ranks_stdlib(input);
+            for i in 0..actual.len() {
+                assert_eq!(actual[i], String::from(expected[i]));
+            }
+        }
+    }
+
+    #[test]
+    fn test_find_relative_ranks_collection() {
+        let test_cases: Vec<(Vec<i32>, Vec<&str>)> = vec![
+            (
+                vec![5, 4, 3, 2, 1],
+                vec!["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"],
+            ),
+            (
+                vec![10, 3, 8, 9, 4],
+                vec!["Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"],
+            ),
+        ];
+
+        for (input, expected) in test_cases {
+            let actual = find_relative_ranks_collection(input);
             for i in 0..actual.len() {
                 assert_eq!(actual[i], String::from(expected[i]));
             }
