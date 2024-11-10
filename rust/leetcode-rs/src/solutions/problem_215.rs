cd97f8c8c4c942f27963ae82d39a116ff976ae7e
Minh Hoang Tien <htminh.1001@gmail.com>
2024-11-10 22:21:51 +0700

    chore: add missing test cases for problem 215

--- rust/leetcode-rs/src/solutions/problem_215.rs
+++ rust/leetcode-rs/src/solutions/problem_215.rs
@@ -8,3 +8,21 @@ pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
     }
     result
 }
+
+#[cfg(test)]
+mod test {
+    use super::find_kth_largest;
+
+    #[test]
+    fn test_find_kth_largest() {
+        let test_cases: Vec<(Vec<i32>, i32, i32)> = vec![
+            (vec![3, 2, 1, 5, 6, 4], 2, 5),
+            (vec![3, 2, 3, 1, 2, 4, 5, 5, 6], 4, 4),
+        ];
+
+        for (input, k, expected) in test_cases {
+            let actual = find_kth_largest(input, k);
+            assert_eq!(actual, expected);
+        }
+    }
+}
