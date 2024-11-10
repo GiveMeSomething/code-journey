8bb6c9fc84350f50657a68588ae376e08928db74
Minh Hoang Tien <htminh.1001@gmail.com>
2024-11-10 20:09:10 +0700

    feat: add solution for problem 1046

--- rust/leetcode-rs/src/solutions/problem_1046.rs
+++ rust/leetcode-rs/src/solutions/problem_1046.rs
@@ -1,3 +1,31 @@
+use crate::dsa::max_heap::MaxHeap;
+
 pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
-    0
+    if stones.len() == 1 {
+        return stones[0];
+    }
+
+    if stones.len() == 2 {
+        return (stones[0] - stones[1]).abs();
+    }
+
+    let mut max_heap = MaxHeap::from(stones);
+
+    while max_heap.array.len() > 1 {
+        // Get 2 largest values
+        let a = max_heap.pop_max();
+        let b = max_heap.pop_max();
+
+        let new_stone = (a - b).abs();
+        if new_stone == 0 {
+            continue;
+        }
+        max_heap.insert(new_stone);
+    }
+
+    if max_heap.array.len() == 1 {
+        return max_heap.array[0];
+    }
+
+    return 0;
 }
