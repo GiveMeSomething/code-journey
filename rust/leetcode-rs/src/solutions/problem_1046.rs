b50ebffe44b6e6ed9343faa313adc5f453155081
Minh Hoang Tien <htminh.1001@gmail.com>
2024-11-10 20:12:31 +0700

    chore: add test cases for problem 1046

--- rust/leetcode-rs/src/solutions/problem_1046.rs
+++ rust/leetcode-rs/src/solutions/problem_1046.rs
@@ -29,3 +29,22 @@ pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
 
     return 0;
 }
+
+#[cfg(test)]
+mod test {
+    use super::last_stone_weight;
+
+    #[test]
+    fn test_last_stone_weight() {
+        let test_cases: Vec<(Vec<i32>, i32)> = vec![
+            (vec![2, 7, 4, 1, 8, 1], 1),
+            (vec![1], 1),
+            (vec![1, 100], 99),
+        ];
+
+        for (input, expected) in test_cases {
+            let actual = last_stone_weight(input);
+            assert_eq!(actual, expected);
+        }
+    }
+}
