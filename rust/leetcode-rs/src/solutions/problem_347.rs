fa41173d4efd78e221fc3ead13df3b35192655d7
Minh Hoang Tien <htminh.1001@gmail.com>
2024-11-12 21:31:29 +0700

    fix: update solution to pass edge cases

--- rust/leetcode-rs/src/solutions/problem_347.rs
+++ rust/leetcode-rs/src/solutions/problem_347.rs
@@ -1,5 +1,30 @@
 use std::collections::{BinaryHeap, HashMap};
 
+pub struct NumCount {
+    value: i32,
+    count: i32,
+}
+
+impl PartialEq for NumCount {
+    fn eq(&self, other: &Self) -> bool {
+        self.count.eq(&other.count)
+    }
+}
+
+impl PartialOrd for NumCount {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.count.partial_cmp(&other.count)
+    }
+}
+
+impl Eq for NumCount {}
+
+impl Ord for NumCount {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.count.cmp(&other.count)
+    }
+}
+
 pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let mut count_map: HashMap<i32, i32> = HashMap::new();
     for num in nums {
@@ -11,16 +36,15 @@ pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
         count_map.insert(num, current_count + 1);
     }
 
-    let mut number_map: HashMap<i32, i32> = HashMap::new();
-    let mut max_heap: BinaryHeap<i32> = BinaryHeap::new();
-    for (key, value) in count_map {
-        number_map.insert(value, key);
-        max_heap.push(value);
+    let mut max_heap: BinaryHeap<NumCount> = BinaryHeap::new();
+    for (value, count) in count_map {
+        max_heap.push(NumCount { value, count });
     }
 
     let mut result: Vec<i32> = vec![];
     for _ in 0..k {
-        result.push(*number_map.get(&max_heap.pop().unwrap()).unwrap());
+        let num_count = max_heap.pop().unwrap();
+        result.push(num_count.value);
     }
     result
 }
@@ -34,6 +58,7 @@ mod test {
         let test_cases: Vec<(Vec<i32>, i32, Vec<i32>)> = vec![
             (vec![1, 1, 1, 2, 2, 3], 2, vec![1, 2]),
             (vec![1], 1, vec![1]),
+            (vec![1, 2], 2, vec![1, 2]),
         ];
 
         for (nums, k, expected) in test_cases {
