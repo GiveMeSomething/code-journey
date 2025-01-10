a9480801d8263a8f38e03c13097866553ff458ed
Hoang Tien Minh <minh.hoang@icetea.io>
2025-01-10 11:15:08 +0700

    chore: add thought process and some explanation for problem 395

--- rust/leetcode-rs/src/solutions/problem_395.rs
+++ rust/leetcode-rs/src/solutions/problem_395.rs
@@ -1,5 +1,31 @@
 use std::{cmp::max, collections::HashMap};
 
+// Thought process:
+//
+// Problem:
+// Given a string s and an integer k,
+// return the length of the longest substring of s,
+// such that the frequency of each character in this substring
+// is GTE to k.
+
+// Hint: Sliding window problem
+// For sliding window problem that trying to find the maximum,
+// we always try to increase the substring length until we cannot increase it without violating requirement
+// Then we decrease it to satisfy the requirement
+
+// First attempt: We try to increase the window until it satisfy the requirement,
+// then decrease it if current does not satisfy the requirement
+// Problem: The current substring does not always satisfy the requirement, so the start is always increase with the end
+
+// When this happened, we should look for another requirement that in turn can satisfy the main problem
+// For this problem, the requirement is that for a substring with n unique character, how many of them can satisfy the requirement
+// For instance, for a substring with only 1 unique character, how many of them have more than k occurence?
+// e.g. "aaa" contains only "a" and "a" have 3 occurences. But "aaab" does not fall in this category as it has 2 unique character
+// By finding in each unique character count, we then can deduce the result for the main problem
+
+// IMO, this should be a HARD problem on Leetcode
+// The step to the hidden requirement is very hard to see without any hint
+
 pub fn longest_substring(s: String, k: i32) -> i32 {
     if k > i32::try_from(s.len()).unwrap() {
         return 0;
@@ -17,7 +43,7 @@ pub fn longest_substring(s: String, k: i32) -> i32 {
     let mut max_len = 0;
 
     let unique_count = char_count.into_iter().filter(|v| *v > 0).count();
-    for i in 2..=unique_count {
+    for i in 1..=unique_count {
         let mut start = 0;
         let mut end = i;
 
