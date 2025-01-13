6e6e141e6e3978e106c1103bef155807161e1876
Hoang Tien Minh <minh.hoang@icetea.io>
2025-01-13 10:55:55 +0700

    feat: add solution for problem 1636 with JS

--- javascript/leetcode/index.js
+++ javascript/leetcode/index.js
@@ -1,8 +1,10 @@
 // Driver file
+import { test1636 } from "./solution/problem-1636.js";
 import { test424 } from "./solution/problem-424.js";
 import { test539 } from "./solution/problem-539.js";
-import { test169 } from "./solution/problem_169.js";
+import { test169 } from "./solution/problem-169.js";
 
 // test539();
 // test424();
-test169();
+// test169();
+test1636();
