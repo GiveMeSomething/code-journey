bd547ee999f082c5b966c029bb49cf40562f8a1a
Minh Hoang Tien <htminh.1001@gmail.com>
2025-01-18 22:06:10 +0700

    chore: init new branch to implement binary tree

--- rust/leetcode-rs/src/dsa/binary_tree/btree.rs
+++ rust/leetcode-rs/src/dsa/binary_tree/btree.rs
@@ -16,3 +16,36 @@ impl BTree {
         return 2 * i + 2;
     }
 }
+
+/* Implementation of binary tree */
+
+pub struct BinaryTree {
+    pub root: TreeNode,
+}
+
+pub struct TreeNode {
+    value: isize,
+    parent: TreeNode,
+}
+
+impl BinaryTree {
+    pub fn traverse(root: &TreeNode) {
+        if let Some(node) = &root.left {
+            BinaryTree::traverse(node);
+        }
+        print!("{} ", root.value);
+        if let Some(node) = &root.right {
+            BinaryTree::traverse(node);
+        }
+    }
+
+    pub fn new(value: isize) -> Self {
+        let root = TreeNode {
+            value,
+            left: None,
+            right: None,
+        };
+
+        BinaryTree { root }
+    }
+}
