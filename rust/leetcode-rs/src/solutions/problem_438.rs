611f89b0ecbe2e736b5c80082f6ed366041ba895
Hoang Tien Minh <htminh.1001@gmail.com>
2025-01-20 09:38:42 +0700

    feat: add solution for problem 438 with Rust

---
+++ rust/leetcode-rs/src/solutions/problem_438.rs
@@ -0,0 +1,94 @@
+use std::collections::HashMap;
+
+// Base solution
+pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
+    if p.len() > s.len() {
+        return vec![];
+    }
+
+    // Create a map of all p character
+    let mut p_char_map: HashMap<String, i32> = HashMap::new();
+    for i in 0..p.len() {
+        let count = p_char_map.entry(String::from(&p[i..i + 1])).or_insert(0);
+        *count += 1;
+    }
+
+    let mut start = 0;
+    let mut result: Vec<i32> = vec![];
+    let mut tracking_char_map: HashMap<String, i32> = HashMap::new();
+
+    while start + p.len() <= s.len() {
+        if tracking_char_map.is_empty() {
+            // Init current char_map if empty (first iteration)
+            for i in 0..p.len() {
+                let count = tracking_char_map
+                    .entry(String::from(&s[i..i + 1]))
+                    .or_insert(0);
+                *count += 1;
+            }
+        } else {
+            let count = tracking_char_map
+                .entry(String::from(&s[start + p.len() - 1..start + p.len()]))
+                .or_insert(0);
+            *count += 1;
+        }
+
+        if is_anagram(&tracking_char_map, &p_char_map) {
+            result.push(start as i32);
+        }
+
+        let first_char_count = tracking_char_map
+            .entry(String::from(&s[start..start + 1]))
+            .or_insert(0);
+        *first_char_count -= 1;
+        start += 1;
+    }
+
+    result
+}
+
+fn is_anagram_chars(m1: &[i32; 26], m2: &[i32; 26]) -> bool {
+    for i in 0..m1.len() {
+        if m1[i] != m2[i] {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_anagram(m1: &HashMap<String, i32>, m2: &HashMap<String, i32>) -> bool {
+    for (key, value) in m2 {
+        if !m1.contains_key(key) {
+            return false;
+        }
+        if *m1.get(key).unwrap() != *value {
+            return false;
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod test {
+    use super::find_anagrams;
+
+    #[test]
+    fn test_find_anagrams() {
+        let test_cases: Vec<(&str, &str, Vec<i32>)> = vec![
+            ("cbaebabacd", "abc", vec![0, 6]),
+            ("abab", "ab", vec![0, 1, 2]),
+            ("aa", "bb", vec![]),
+        ];
+
+        for (s, p, expected) in test_cases {
+            let result = find_anagrams(String::from(s), String::from(p));
+
+            println!("TEST CASE {} {}", s, p);
+            assert_eq!(result.len(), expected.len());
+
+            for i in 0..result.len() {
+                assert_eq!(result[i], expected[i]);
+            }
+        }
+    }
+}
