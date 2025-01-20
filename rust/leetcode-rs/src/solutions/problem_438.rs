57434c11ed9317e9eae0d64a05ed798de4377550
Hoang Tien Minh <htminh.1001@gmail.com>
2025-01-20 09:39:10 +0700

    feat: add more optimal solution for problem 438 with Rust

--- rust/leetcode-rs/src/solutions/problem_438.rs
+++ rust/leetcode-rs/src/solutions/problem_438.rs
@@ -1,46 +1,83 @@
 use std::collections::HashMap;
 
 // Base solution
+// pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
+//     if p.len() > s.len() {
+//         return vec![];
+//     }
+
+//     // Create a map of all p character
+//     let mut p_char_map: HashMap<String, i32> = HashMap::new();
+//     for i in 0..p.len() {
+//         let count = p_char_map.entry(String::from(&p[i..i + 1])).or_insert(0);
+//         *count += 1;
+//     }
+
+//     let mut start = 0;
+//     let mut result: Vec<i32> = vec![];
+//     let mut tracking_char_map: HashMap<String, i32> = HashMap::new();
+
+//     while start + p.len() <= s.len() {
+//         if tracking_char_map.is_empty() {
+//             // Init current char_map if empty (first iteration)
+//             for i in 0..p.len() {
+//                 let count = tracking_char_map
+//                     .entry(String::from(&s[i..i + 1]))
+//                     .or_insert(0);
+//                 *count += 1;
+//             }
+//         } else {
+//             let count = tracking_char_map
+//                 .entry(String::from(&s[start + p.len() - 1..start + p.len()]))
+//                 .or_insert(0);
+//             *count += 1;
+//         }
+
+//         if is_anagram(&tracking_char_map, &p_char_map) {
+//             result.push(start as i32);
+//         }
+
+//         let first_char_count = tracking_char_map
+//             .entry(String::from(&s[start..start + 1]))
+//             .or_insert(0);
+//         *first_char_count -= 1;
+//         start += 1;
+//     }
+
+//     result
+// }
+
+// Faster way, we use an array of length 26 to count character instead of HashMap to avoid computation overhead
 pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
     if p.len() > s.len() {
         return vec![];
     }
 
-    // Create a map of all p character
-    let mut p_char_map: HashMap<String, i32> = HashMap::new();
-    for i in 0..p.len() {
-        let count = p_char_map.entry(String::from(&p[i..i + 1])).or_insert(0);
-        *count += 1;
+    let s_chars: Vec<char> = s.chars().collect();
+    let p_chars: Vec<char> = p.chars().collect();
+
+    let mut anagram_map = [0; 26];
+    for char in p_chars {
+        anagram_map[char as usize - 97] += 1;
     }
 
-    let mut start = 0;
+    let mut tracking_map = [0; 26];
     let mut result: Vec<i32> = vec![];
-    let mut tracking_char_map: HashMap<String, i32> = HashMap::new();
-
+    let mut start = 0;
     while start + p.len() <= s.len() {
-        if tracking_char_map.is_empty() {
-            // Init current char_map if empty (first iteration)
-            for i in 0..p.len() {
-                let count = tracking_char_map
-                    .entry(String::from(&s[i..i + 1]))
-                    .or_insert(0);
-                *count += 1;
+        if start == 0 {
+            for i in 0..p.len() - 1 {
+                tracking_map[s_chars[i] as usize - 97] += 1;
             }
-        } else {
-            let count = tracking_char_map
-                .entry(String::from(&s[start + p.len() - 1..start + p.len()]))
-                .or_insert(0);
-            *count += 1;
         }
 
-        if is_anagram(&tracking_char_map, &p_char_map) {
+        tracking_map[s_chars[start + p.len() - 1] as usize - 97] += 1;
+
+        if is_anagram_chars(&tracking_map, &anagram_map) {
             result.push(start as i32);
         }
 
-        let first_char_count = tracking_char_map
-            .entry(String::from(&s[start..start + 1]))
-            .or_insert(0);
-        *first_char_count -= 1;
+        tracking_map[s_chars[start] as usize - 97] -= 1;
         start += 1;
     }
 
