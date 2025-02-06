7855e70e1daaafafa66539ebbde57fe1088adbda
Hoang Tien Minh <minh.hoang@icetea.io>
2025-02-06 14:14:28 +0700

    feat: add solution foc aoc24 day2 part1

--- rust/advent-of-code-2024/src/main.rs
+++ rust/advent-of-code-2024/src/main.rs
@@ -1,6 +1,10 @@
 mod solutions;
 
 fn main() {
-    solutions::one::part_1();
-    solutions::one::part_2();
+    // solutions::one::part_1();
+    // solutions::one::part_2();
+
+    let safe_report_count = solutions::two::part_1();
+
+    println!("{}", safe_report_count);
 }
