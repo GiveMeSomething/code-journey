1ad7028d6dc38d4f71594540e4758c727f50483d
Hoang Tien Minh <htminh.1001@gmail.com>
2025-02-18 13:26:55 +0700

    fix: add more test case for palindromic substring set + fix substring() errors

--- rust/leetcode-rs/src/random/palindromic_permutation.rs
+++ rust/leetcode-rs/src/random/palindromic_permutation.rs
@@ -15,7 +15,7 @@ pub fn palindromic_permutation(inputs: Vec<String>) -> usize {
 }
 
 // Generate all possible permutation
-pub fn recur_palindromize_permutation(
+fn recur_palindromize_permutation(
     inputs: &Vec<String>,
     result: &mut HashSet<String>,
     current: String,
@@ -37,17 +37,18 @@ pub fn recur_palindromize_permutation(
     }
 }
 
-pub fn substring(input: String) -> Vec<String> {
-    let mut result: Vec<String> = vec![];
+fn substring(input: String) -> Vec<String> {
+    let mut result: HashSet<String> = HashSet::new();
     for i in 0..input.len() {
         for j in i..input.len() {
-            result.push(String::from(&input[i..=j]));
+            result.insert(String::from(&input[i..=j]));
         }
     }
-    result
+
+    result.into_iter().collect()
 }
 
-pub fn is_palindrome(input: &str) -> bool {
+fn is_palindrome(input: &str) -> bool {
     let chars: Vec<char> = input.chars().collect();
     let mut start = 0;
     let mut end = input.len() - 1;
@@ -66,7 +67,9 @@ pub fn is_palindrome(input: &str) -> bool {
 
 #[cfg(test)]
 mod test {
-    use super::palindromic_permutation;
+    use std::collections::HashSet;
+
+    use super::{is_palindrome, palindromic_permutation, substring};
 
     #[test]
     fn test_palindromic_permutation() {
@@ -79,6 +82,35 @@ mod test {
         for (inputs, expected) in test_cases {
             let result = palindromic_permutation(inputs.iter().map(|v| String::from(*v)).collect());
             assert_eq!(result, expected);
+        }
+    }
+
+    #[test]
+    fn test_is_palindrome() {
+        let test_cases: Vec<(&str, bool)> = vec![("aba", true), ("abba", true), ("abc", false)];
+
+        for (input, expected) in test_cases {
+            let result = is_palindrome(input);
+            assert_eq!(result, expected);
+        }
+    }
+
+    #[test]
+    fn test_substring() {
+        let test_cases: Vec<(&str, Vec<&str>)> = vec![
+            ("abc", vec!["a", "b", "c", "ab", "abc", "bc"]),
+            ("aaa", vec!["a", "aa", "aaa"]),
+        ];
+
+        for (input, expected) in test_cases {
+            let result = substring(String::from(input));
+            let expected_set: HashSet<String> =
+                HashSet::from_iter(expected.iter().map(|v| String::from(*v)));
+
+            assert_eq!(result.len(), expected_set.len());
+            for substring in result {
+                assert!(expected_set.contains(&substring));
+            }
         }
     }
 }
