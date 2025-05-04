da7a463a4a5483679b82c18ac8f94fb23965033b
Hoang Tien Minh <htminh.1001@gmail.com>
2025-05-04 21:02:57 +0700

    chore: init new Python project for AoC 2021

---
+++ python/advent-of-code-2021/src/__init__.py
@@ -0,0 +1,8 @@
+# This file marks the directory as a Python package.
+
+# Optionally, you can define package-level imports
+# from .module_name import some_function, some_class
+
+# You can also define metadata
+__version__ = "0.1.0"
+__author__ = "Advent of Code 2021"
