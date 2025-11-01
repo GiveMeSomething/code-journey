359122493dce5c3518e662f07ca09151dc83efbb
Hoang Tien Minh <htminh.1001@gmail.com>
2025-11-02 00:09:17 +0700

    feat: add normalization for x and y

--- python/machine-learning/utils/read_csv.py
+++ python/machine-learning/utils/read_csv.py
@@ -7,42 +7,65 @@ def open_training_dataset(input_path):
 
 
 class CustomDataset:
+    data = pd.DataFrame(None)
     x_matrix = []
     y_vector = []
 
+    # Denormalize parameters for each columns
+    # (min, max)[]
+    denormalize_params_x = []
+    denormalize_params_y = (0, 0)
+
     def __init__(self, input_path):
         data = open_training_dataset(input_path)
-        cols = data.shape[1]
-
-        # Assume that there're n columns in the dataset
-        # First n-1 columns will be training features
-        # Last column (n-th) will be training targets
+        cols = self.data.shape[1]
 
         # Normalize x_matrix using min/max
-        self.x_matrix = data.iloc[:, 0 : cols - 1].to_numpy(dtype=np.float32)
+        x_matrix = data.iloc[:, 0 : cols - 1].to_numpy(dtype=np.float32)
+        ones = np.full((data.shape[0], 1), 1, dtype=np.float32)
+        x_matrix = np.hstack((ones, x_matrix))
 
-        # for i in range(data.shape[1] - 1):
-        #     current_col = data.iloc[:, i].to_numpy(dtype=np.float32)
-        #     col_min = np.min(current_col)
-        #     col_max = np.max(current_col)
-        #     for j in range(len(current_col)):
-        #         if col_max - col_min == 0:
-        #             self.x_matrix[j][i] = 0
-        #             continue
-        #         self.x_matrix[j][i] = (self.x_matrix[j][i] - col_min) / (
-        #             col_max - col_min
-        #         )
+        y_vector = data.iloc[:, -1].to_numpy()
 
-        self.x_matrix = np.hstack(
-            (np.ones((data.shape[0], 1), dtype=np.float32), self.x_matrix)
-        )
+        self.data = data
+        self.x_matrix = x_matrix
+        self.y_vector = y_vector
 
-        # Normalize y vector using min/max
-        self.y_vector = data.iloc[:, -1].to_numpy()
-        # y_min = np.min(self.y_vector)
-        # y_max = np.max(self.y_vector)
-        # for i in range(self.y_vector.shape[0]):
-        #     if y_max - y_min == 0:
-        #         self.y_vector[i] = 0
-        #         continue
-        #     self.y_vector[i] = (self.y_vector[i] - y_min) / (y_max - y_min)
+    def normalize_x(self):
+        # Do normalize feature for all feature
+        for i in range(self.x_matrix.shape[1]):
+            self.denormalize_params_x.append((0, 1))
+            # Skip x0 = 1 columns
+            if i == 0:
+                continue
+
+            # Use min/max normalization
+            current_col = self.data.iloc[:, i].to_numpy(dtype=np.float32)
+            col_min = np.min(current_col)
+            col_max = np.max(current_col)
+            min_max_diff = col_max - col_min
+            self.denormalize_params_x[i] = (col_min, col_max)
+            for j in range(len(current_col)):
+                if min_max_diff == 0:
+                    self.x_matrix[j][i] = 1
+                self.x_matrix[j][i] = (self.x_matrix[j][i] - col_min) / min_max_diff
+
+    def normalize_y(self):
+        y_min = np.min(self.y_vector)
+        y_max = np.max(self.x_matrix)
+        min_max_diff = y_max - y_min
+        self.denormalize_params_y = (y_min, y_max)
+        for i in range(len(self.y_vector)):
+            self.y_vector[i] = (self.y_vector[i] - y_min) / min_max_diff
+
+    # Denormalize a row of x values
+    def denormalize_x(self, x_vector):
+        result = []
+        for i in range(len(x_vector)):
+            min, max = self.denormalize_params_x[i]
+            result[i] = x_vector[i] * (max - min) + min
+        return result
+
+    def denormalize_y(self, y):
+        min, max = self.denormalize_params_y
+        return y * (max - min) + min
