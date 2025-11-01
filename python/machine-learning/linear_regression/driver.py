e044031454c120e66cbd08053b8d08fd5006e142
Hoang Tien Minh <htminh.1001@gmail.com>
2025-11-02 00:09:52 +0700

    fix: decouple driver functions

--- python/machine-learning/linear_regression/driver.py
+++ python/machine-learning/linear_regression/driver.py
@@ -1,4 +1,3 @@
-import numpy as np
 from linear_regression.linear_regression import train, predict_with_model
 from utils.read_csv import CustomDataset
 
@@ -6,16 +5,15 @@ TRAINING_DATA_PATH = "linear_regression/training.csv"
 TESTING_DATA_PATH = "linear_regression/testing.csv"
 
 
-def train_model():
-    dataset = CustomDataset(TRAINING_DATA_PATH)
-    model_theta_vector = train(dataset.x_matrix, dataset.y_vector, 1_000_000)
+def train_model(dataset: CustomDataset):
+    model_theta_vector = train(dataset.x_matrix, dataset.y_vector)
 
     print(f"Training model: {model_theta_vector}")
 
     return model_theta_vector
 
 
-def test_read_data():
+def read_data():
     training_dataset = CustomDataset(TRAINING_DATA_PATH)
     print(
         f"Training dataset: x_matrix = {training_dataset.x_matrix} y_vector = {training_dataset.y_vector}"
@@ -25,27 +23,19 @@ def test_read_data():
     print(
         f"Testing dataset: x_matrix = {testing_dataset.x_matrix} y_vector = {testing_dataset.y_vector}"
     )
+    return (training_dataset, testing_dataset)
 
 
-def test_model(model):
-    dataset = CustomDataset(TESTING_DATA_PATH)
-
+def test_model(model, dataset: CustomDataset):
     prediction = list(
         map(lambda x_vector: predict_with_model(model, x_vector), dataset.x_matrix)
     )
+    print(f"Prediction vector {list(map(dataset.denormalize_y, prediction))}")
 
-    print(prediction)
-
-    for i in range(dataset.x_matrix.shape[0]):
-        accuracy = 1 - (abs(prediction[i] - dataset.y_vector[i]) / dataset.y_vector[i])
-        print(
-            f"Dataset {dataset.x_matrix[i]} Target {dataset.y_vector[i]} Prediction {prediction[i]} Accuracy {accuracy * 100}"
-        )
     accuracy = (
         sum(
             map(
-                lambda hypothesis, target: (1 - abs(hypothesis - target) / target)
-                * 100,
+                lambda hypothesis, target: abs(hypothesis - target) ** 2,
                 prediction,
                 dataset.y_vector,
             )
@@ -53,10 +43,16 @@ def test_model(model):
         / dataset.x_matrix.shape[0]
     )
 
-    print(f"Model accuracy: {accuracy}%")
+    print(f"Model mean square error: {accuracy}")
 
 
 def start_linear_regression_driver():
-    test_read_data()
-    model = train_model()
-    test_model(model)
+    training_dataset, testing_dataset = read_data()
+    training_dataset.normalize_x()
+    training_dataset.normalize_y()
+
+    model = train_model(training_dataset)
+
+    testing_dataset.normalize_x()
+    testing_dataset.normalize_y()
+    test_model(model, testing_dataset)
