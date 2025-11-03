399a4d14b6be459df9d312f30a2cd19ea396f1c9
Hoang Tien Minh <htminh.1001@gmail.com>
2025-11-03 22:36:30 +0700

    chore: add result for house price prediction demo

--- python/machine-learning/main.py
+++ python/machine-learning/main.py
@@ -1,5 +1,6 @@
 from predict_function.driver import start_predict_function
-
+from predict_house_pricing.driver import start_predict_house_pricing
 
 if __name__ == "__main__":
-    start_predict_function()
+    # start_predict_function()
+    start_predict_house_pricing()
